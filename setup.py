8ba77c03a617f63b60c3d6b609680e484085fc52
km4ege <magda14@vt.edu>
2018-07-27 18:15:03 -0400

    Allow module to be called outside library.

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup 
+
+setup(name='eclipse_calculator', 
+        version='0.0.1', 
+        description='Eclipse Obscuration Calculator.',
+        author='Nathaniel Frissell',
+        packages=['eclipse_calc'],
+        )
